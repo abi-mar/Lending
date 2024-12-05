f0788ee4a432965015977b0fcdec56caa20b7f1c
unknown <abraham.s.maravillas@gmail.com>
2024-12-05 14:32:01 +0800

    Initial Routes added

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,28 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('lending', 'MainController::index');
+
+
+
+
+// Dashboard routes
+// $routes->get('lending/', 'DashboardController::index');
+
+// Loan routes
+$routes->get('lending/loan', 'LoanController::index');
+$routes->get('lending/loan/(:any)', 'LoanController::showAmount/$1');
+
+// User routes
+$routes->get('lending/login', 'UserController::login');
+
+// Customer routes
+$routes->get('lending/customer', 'CustomerController::index');
+
+
+// Groups
+// $routes->group('lending/loan', function ($routes) {
+//     $routes->get('users', 'Admin\Users::index');
+//     $routes->get('blog', 'Admin\Blog::index');
+// });
