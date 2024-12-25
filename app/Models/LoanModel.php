84b073001e8978921aa6f22c6a67baa7cedba64e
unknown <abraham.s.maravillas@gmail.com>
2024-12-25 19:06:52 +0800

    LoanModel added

---
+++ app/Models/LoanModel.php
@@ -0,0 +1,14 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class LoanModel extends Model {
+    protected $table = 'loan_record';
+    protected $primaryKey = 'row_id';
+    protected $allowedFields = [
+        'custno', 'loan_date', 'loan_amount', 'net_proceeds', 'weekly_amortization', 'amount_topay', 'added_by'
+    ];
+
+}
+
+?>
