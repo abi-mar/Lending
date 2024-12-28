1c114f1e3aa88ac90f2da671357aa24eb12c4d9a
unknown <abraham.s.maravillas@gmail.com>
2024-12-28 22:32:35 +0800

    1. Added badges UI for N/A, Yes and No texts. 2. Is Overdue column added

--- app/Views/payment/index.php
+++ app/Views/payment/index.php
@@ -16,17 +16,18 @@
 
             <div class="card">
                 <div class="card-header">
-                    <h4>Loan Records</h4>
+                    <h4>Payments per Loan</h4>
                 </div>
                 <div class="card-body">
-                    <table id="PaymentsPerTable" class="table table-striped">
+                    <table id="PaymentsPerLoanTable" class="table table-striped">
                         <thead>
                             <tr>
                                 <th>Loan ID</th>
                                 <th>Amount</th>
                                 <th>Date Paid</th>
-                                <th>Scheduled Date</th>
+                                <th>Scheduled Payment</th>
                                 <th>Is Paid?</th>
+                                <th>Is Overdue?</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -35,12 +36,29 @@
                                 <?php foreach($payments as $row) : ?>
                                 <tr>
                                     <td><?php echo $row['load_record_row_id']; ?></td>                                    
-                                    <td><?php echo $row['amount']; ?></td>                                    
-                                    <td><?php echo $row['date_paid']; ?></td>
+                                    <td><?= (isset($row['date_paid'])) ? $row['amount'] : '<span class="badge text-bg-secondary">N/A</span>'; ?></td>
+                                    <td><?= (isset($row['date_paid'])) ? $row['date_paid'] : '<span class="badge text-bg-secondary">N/A</span>'; ?></td>
                                     <td><?php echo $row['scheduled_date']; ?></td>
-                                    <td><?= ($row['is_paid'] == 0) ? '<i class="fa-solid fa-square-xmark"></i>': '<i class="fa-solid fa-square-check"></i>'; ?></td>
+                                    <td><?= ($row['is_paid'] == 1) ? '<span class="badge text-bg-success">Yes</span>': '<span class="badge text-bg-danger">No</span>'; ?></td>
                                     <td>
-                                        <a href="#" class="btn btn-primary btn-sm">Payments</a>                                        
+                                        <?php
+                                            $scheduled_date = new DateTime($row['scheduled_date']);
+                                            $current_date = new DateTime();
+                                            if (isset($row['date_paid'])) {
+                                                echo '<span class="badge text-bg-secondary">N/A</span>';
+                                            } else {
+                                                echo ($scheduled_date < $current_date) ? '<span class="badge text-bg-success">Yes</span>' : '<span class="badge text-bg-danger">No</span>';
+                                            }
+                                        ?>
+                                    </td>
+                                    <td>
+                                        <?php 
+                                            if (isset($row['date_paid']) || ($scheduled_date > $current_date)) {
+                                                echo '<a href="#" class="btn btn-secondary btn-sm">Make Payment</a>';
+                                            } else {
+                                                echo '<a href="#" class="btn btn-primary btn-sm">Make Payment</a>';
+                                            }
+                                        ?>                                        
                                     </td>
                                 </tr>
                                 <?php endforeach; ?>
@@ -57,9 +75,7 @@
 <!-- Initialize DataTables -->
 <script>
     $(document).ready(function() {
-        $('#loanTable').DataTable({
-            "order": [[0, "desc"]]
-        });
+        $('#PaymentsPerLoanTable').DataTable();
     });
 </script>
 <?=$this->endSection()?>
