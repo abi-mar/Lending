e98319c3a86ecef2dd7d9d2d54ea04c63d3bfb1d
unknown <abraham.s.maravillas@gmail.com>
2025-01-04 15:39:49 +0800

    Report routes added

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,9 +29,10 @@ $routes->get('lending/loan', 'LoanController::index', ['filter' => 'authFilter']
 //$routes->get('lending/loan/create/(:num)', 'LoanController::create/$1', ['filter' => 'authFilter']);
 $routes->get('lending/loan/create', 'LoanController::create', ['filter' => 'authFilter']);
 $routes->post('lending/loan/add', 'LoanController::add', ['filter' => 'authFilter']);
+$routes->get('lending/loan/edit/(:num)', 'LoanController::edit/$1', ['filter' => 'authFilter']);
+$routes->post('lending/loan/update/(:num)', 'LoanController::update/$1', ['filter' => 'authFilter']);
 $routes->get('lending/loan/delete/(:num)', 'LoanController::delete/$1', ['filter' => 'authFilter']);
-$routes->get('lending/loan/getLoanRecords/(:num)', 'LoanController::getLoansByCustomer/$1');
-
+$routes->get('lending/loan/getLoanRecordOfCustomer/(:num)', 'LoanController::getLoanByCustomer/$1');
 
 // Payment routes
 $routes->get('lending/payment', 'PaymentController::index', ['filter' => 'authFilter']);
@@ -39,6 +40,11 @@ $routes->get('lending/payment/perLoan/(:num)', 'PaymentController::perLoan/$1', 
 $routes->get('lending/payment/make', 'PaymentController::makePayment', ['filter' => 'authFilter']);
 $routes->get('lending/payment/make/(:num)', 'PaymentController::makePaymentPerLoan/$1', ['filter' => 'authFilter']);
 $routes->post('lending/payment/add', 'PaymentController::add', ['filter' => 'authFilter']);
+$routes->get('lending/payment/getPaymentsByBatch', 'PaymentController::getRecordsByBatch', ['filter' => 'authFilter']);
+
+// Report routes
+$routes->get('lending/report', 'ReportController::index', ['filter' => 'authFilter']);
+$routes->get('lending/report/generate', 'ReportController::generateReport', ['filter' => 'authFilter']);
 
 
 // Groups
